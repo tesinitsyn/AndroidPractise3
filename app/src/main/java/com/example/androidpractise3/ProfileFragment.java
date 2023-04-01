3891c1f8de0f0cb22ec5f579ae8a12bea8267a5b
tesinitsyn <sitiev174@gmail.com>
2023-04-01 10:55:32 +0300

    manifest fixed, additional code in fragments

--- app/src/main/java/com/example/androidpractise3/ProfileFragment.java
+++ app/src/main/java/com/example/androidpractise3/ProfileFragment.java
@@ -1,17 +1,18 @@
 package com.example.androidpractise3;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 
 public class ProfileFragment extends Fragment {
@@ -33,7 +34,89 @@ public class ProfileFragment extends Fragment {
         return inflater.inflate(R.layout.activity_profile, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView nameView = view.findViewById(R.id.sporstmanName);
+        TextView surnameView = view.findViewById(R.id.sporstmanSurname);
+        TextView emailView = view.findViewById(R.id.sportmanEmail);
 
+        if (getArguments() != null) {
+            Bundle bundle = getArguments();
+            nameView.setText(bundle.get("name").toString());
+            surnameView.setText(bundle.get("surname").toString());
+            emailView.setText(bundle.get("email").toString());
+            Log.i(TAG, "Data Received");
+        }
+
+        Button returnButton = view.findViewById(R.id.return_button);
+        returnButton.setOnClickListener(vie -> {
+            Bundle backTransferData = new Bundle();
+            backTransferData.putString("name", nameView.getText().toString() + "go back");
+            backTransferData.putString("surname", surnameView.getText().toString());
+            backTransferData.putString("email", emailView.getText().toString());
+            RegistrationFragment registrationFragment = (RegistrationFragment) requireActivity()
+                    .getSupportFragmentManager()
+                    .findFragmentByTag(RegistrationFragment.TAG);
+            if (registrationFragment != null) {
+                System.out.println("if worked");
+                registrationFragment.setArguments(backTransferData);
+                requireActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container_view, registrationFragment)
+                        .commit();
+            }
+            Log.i(TAG, "button pressed");
+        });
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Created");
+        Toast.makeText(getContext(), "Created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "Started");
+        Toast.makeText(getContext(), "Started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "Paused");
+        Toast.makeText(getContext(), "Paused", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach");
+        Toast.makeText(getContext(), "onAttach", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG, "onDetach");
+        Toast.makeText(getContext(), "onDetach", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "Stopped");
+        Toast.makeText(getContext(), "Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Destroyed");
+        Toast.makeText(getContext(), "Destroyed", Toast.LENGTH_SHORT).show();
+    }
 
 
 }
--- app/src/main/java/com/example/androidpractise3/RegistrationFragment.java
+++ app/src/main/java/com/example/androidpractise3/RegistrationFragment.java
@@ -1,21 +1,21 @@
 package com.example.androidpractise3;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
 
 
 public class RegistrationFragment extends Fragment {
@@ -43,22 +43,22 @@ public class RegistrationFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-       name = (TextInputEditText) view.findViewById(R.id.Name);
-       surname = (TextInputEditText) view.findViewById(R.id.surname);
-       email = (TextInputEditText) view.findViewById(R.id.email);
+       name = view.findViewById(R.id.Name);
+       surname = view.findViewById(R.id.surname);
+       email = view.findViewById(R.id.email);
 
-       Button submitButton = (Button) view.findViewById(R.id.submit_button);
+       Button submitButton = view.findViewById(R.id.submit_button);
 
        submitButton.setOnClickListener(vie -> {
            Bundle userData = new Bundle();
-           userData.putString("name", name.getText().toString());
-           userData.putString("surname", surname.getText().toString());
-           userData.putString("email", email.getText().toString());
+           userData.putString("name", Objects.requireNonNull(name.getText()).toString());
+           userData.putString("surname", Objects.requireNonNull(surname.getText()).toString());
+           userData.putString("email", Objects.requireNonNull(email.getText()).toString());
 
            ProfileFragment profileFragment = ProfileFragment.newInstance();
            profileFragment.setArguments(userData);
            requireActivity().getSupportFragmentManager().beginTransaction()
-                   .replace(R.id.fragment_container_view, ProfileFragment)
+                   .replace(R.id.fragment_container_view, profileFragment)
                    .addToBackStack(TAG)
                    .commit();
 
@@ -67,5 +67,88 @@ public class RegistrationFragment extends Fragment {
        Log.i(TAG, "onViewCreated");
        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_SHORT).show();
     }
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.i(TAG, "onViewStateRestored");
+        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "Attached");
+        Toast.makeText(getContext(), "Attached", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Created");
+        Toast.makeText(getContext(), "Created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if(getArguments() != null){
+            Bundle dataFromSecond = getArguments();
+            name.setText(dataFromSecond.get("name").toString());
+            surname.setText(dataFromSecond.get("surname").toString());
+            email.setText(dataFromSecond.get("email").toString());
+        }
+
+        Log.i(TAG, "onResume");
+        Toast.makeText(getContext(), "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "Started");
+        Toast.makeText(getContext(), "Started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG, "onDestroyView");
+        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "Paused");
+        Toast.makeText(getContext(), "Paused", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "Stopped");
+        Toast.makeText(getContext(), "Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Destroyed");
+        Toast.makeText(getContext(), "Destroyed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG, "onDetach");
+        Toast.makeText(getContext(), "onDetach", Toast.LENGTH_SHORT).show();
+    }
 }
